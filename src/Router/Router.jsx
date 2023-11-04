6eac4bde21b7c5bb4024b0faebc4ed9d76482155
alsafa012 <alsafa012@gmail.com>
2023-11-04 21:47:33 +0600

    raect router dom implemented

---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const myCreatedRouter = createBrowserRouter([
+     {
+          path: "/",
+          element: <div>Hello world!</div>,
+     },
+]);
+
+export default myCreatedRouter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const router = createBrowserRouter([
-     {
-          path: "/",
-          element: <div>Hello world!</div>,
-     },
-]);
+import { RouterProvider } from "react-router-dom";
+import myCreatedRouter from "./Router/Router";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
      <React.StrictMode>
-          <RouterProvider router={router} />
+          <RouterProvider router={myCreatedRouter} />
      </React.StrictMode>
 );
