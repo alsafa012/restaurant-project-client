de361d5b227e07cafdf73a584d7509e45dd0228e
alsafa012 <alsafa012@gmail.com>
2023-11-06 00:10:05 +0600

    counter section added successfully

--- src/Components/CountingSection/Counter.jsx
+++ src/Components/CountingSection/Counter.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Counter = () => {
+     const [scrollTrigger, setScrollTrigger] = useState(false);
      return (
-          <div>
-               counter
+          <div className="container mx-auto ">
+               <ScrollTrigger
+                    onEnter={() => setScrollTrigger(true)}
+                    onExit={() => setScrollTrigger(false)}
+               >
+                    <div className="border-5 border-red-700 relative w-full">
+                    <div className=" h-[300px] bg-[url('https://i.ibb.co/1Xs8qWX/counter-bg.jpg')] bg-cover bg-center opacity-70"></div>
+                         <div className="absolute top-4 space-y-3 md:space-y-0 md:top-[45%] w-full md:flex md:justify-around text-center md:items-center md:gap-5">
+                              <div>
+                                   <h2 className="text-3xl md:text-4xl font-bold text-black">
+                                        {scrollTrigger && (
+                                             <CountUp
+                                             className="text-[#FF444A]"
+                                                  start={0}
+                                                  end={65}
+                                                  duration={2.75}
+                                             />
+                                        )}
+                                        +
+                                   </h2>
+                                   <p className="text-3xl md:text-4xl font-bold text-black">Reviews</p>
+                              </div>
+                              <div>
+                                   <h2 className="text-3xl md:text-4xl font-bold text-black">
+                                        <span>Up to</span>
+                                        {scrollTrigger && (
+                                             <CountUp
+                                             className="text-[#FF444A] ml-3"
+                                                  start={0}
+                                                  end={50}
+                                                  duration={2.75}
+                                             />
+                                        )}
+                                        +
+                                   </h2>
+                                   <p className="text-3xl md:text-4xl font-bold text-black">Food Items </p>
+                              </div>
+                              <div>
+                                   <h2 className="text-3xl md:text-4xl font-bold text-black">
+                                        {scrollTrigger && (
+                                             <CountUp
+                                             className="text-[#FF444A]"
+                                                  start={0}
+                                                  end={100}
+                                                  duration={2.75}
+                                             />
+                                        )}
+                                        %
+                                   </h2>
+                                   <p className="text-3xl md:text-4xl font-bold text-black">Hygienic</p>
+                              </div>
+                         </div>
+                    </div>
+                    {/* </div> */}
+               </ScrollTrigger>
           </div>
      );
 };
