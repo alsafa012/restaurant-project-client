0b8d7cb88b59d2a4b9c8cdb46e0348474c4ff00a
alsafa012 <alsafa012@gmail.com>
2023-11-08 20:57:12 +0600

    banner page updated

--- src/Components/HeaderSection/BannerSection/BannerSection.jsx
+++ src/Components/HeaderSection/BannerSection/BannerSection.jsx
@@ -15,7 +15,7 @@ const BannerSection = () => {
                                    src="https://i.ibb.co/mq4RZCz/ifs-hero-food-still-life-1920x1080x72.jpg"
                                    className="object-cover w-full "
                               />
-                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] w-full">
+                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 w-full">
                                    <div className="mx-auto">
                                         <div className="text-white space-y-3  text-center  ">
                                              <h3 className=" text-3xl md:text-4xl lg:text-5xl mb-5 font-bold">
@@ -57,7 +57,7 @@ const BannerSection = () => {
                                    src=" https://i.ibb.co/dcH28jR/1647973541988-restaurant-banner.png"
                                    className="object-cover w-full "
                               />
-                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] w-full">
+                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 w-full">
                                    <div className="mx-auto">
                                         <div className="text-white space-y-3  text-center  ">
                                              <h3 className=" text-3xl md:text-4xl lg:text-5xl mb-5 font-bold">
@@ -100,7 +100,7 @@ const BannerSection = () => {
                                    // src="https://i.ibb.co/tBTvLZd/sam-moghadam-khamseh-wg551-YMfe-Oc-unsplash.jpg"
                                    className="w-full object-cover"
                               />
-                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] w-full">
+                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 w-full">
                                    <div className="mx-auto">
                                         <div className="text-white space-y-3  text-center  ">
                                              <h3 className=" text-3xl md:text-4xl lg:text-5xl mb-5 font-bold">
@@ -139,7 +139,7 @@ const BannerSection = () => {
                                    src="https://i.ibb.co/y5ZwL1N/burger-banner2.jpg"
                                    className="object-cover w-full "
                               />
-                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)] w-full">
+                              <div className="absolute h-full flex gap-4 items-center top-0 left-0 w-full">
                                    <div className="mx-auto">
                                         <div className="text-white space-y-3  text-center  ">
                                              <h3 className=" text-3xl md:text-4xl lg:text-5xl mb-5 font-bold">
